dd1198d88757942e898d246f234feff9c93bf31d
Glitch (flicker-enchanting-microwave) <none>
2020-07-26 19:09:54 +0000

    working copy. Meaning this works.

--- README.md
+++ README.md
@@ -1,16 +1,14 @@
 # API Project: URL Shortener Microservice for freeCodeCamp
 
-
 ### User Stories
 
 1. I can POST a URL to `[project_url]/api/shorturl/new` and I will receive a shortened URL in the JSON response. Example : `{"original_url":"www.google.com","short_url":1}`
-2. If I pass an invalid URL that doesn't follow the valid `http(s)://www.example.com(/more/routes)` format, the JSON response will contain an error like `{"error":"invalid URL"}`. *HINT*: to be sure that the submitted url points to a valid site you can use the function `dns.lookup(host, cb)` from the `dns` core module.
+2. If I pass an invalid URL that doesn't follow the valid `http(s)://www.example.com(/more/routes)` format, the JSON response will contain an error like `{"error":"invalid URL"}`. _HINT_: to be sure that the submitted url points to a valid site you can use the function `dns.lookup(host, cb)` from the `dns` core module.
 3. When I visit the shortened URL, it will redirect me to my original link.
-
 
 #### Creation Example:
 
-POST [project_url]/api/shorturl/new - body (urlencoded) :  url=https://www.google.com
+POST [project_url]/api/shorturl/new - body (urlencoded) : url=https://www.google.com
 
 #### Usage:
 
@@ -19,3 +17,11 @@ POST [project_url]/api/shorturl/new - body (urlencoded) :  url=https://www.googl
 #### Will redirect to:
 
 https://www.freecodecamp.org/forum/
+
+# Notes to Self
+
+### VS Code, Github, & Glitch
+
+Today (7/25/2020) I actually used the (apparently previously) installed Glitch VS Code Extension. It was incredibly easy and conveneient. I realized I can write code to my Glitch project now on my computer and in the browser (even from my iPad).
+Also I tried out the import/export to Github. Currently this project lives at aarongilly/glitch. It works well, but does require pull reuessts and it's instantly synced like the VS Code Extension is. However this does let me work within the iPad app "Working Copy", but it's not really clear if that's a better experience than just writing to Glitch directly from within the Safari on the iPad, like I'm doing now.
+I got a basic write new document script working. So that's cool. Not sure why I was so scared to start.
--- server.js
+++ server.js
@@ -1,55 +1,134 @@
 'use strict';
 
 var express = require('express');
-var mongo = require('mongodb');
 var mongoose = require('mongoose');
-
+var dns = require('dns');
 var cors = require('cors');
-
 var app = express();
-
-var fs = require('fs');
-var path = require('path');
 var bodyParser = require('body-parser');
-var router = express.Router();
 
 // Basic Configuration 
 var port = process.env.PORT || 3000;
 
 /** this project needs a db !! **/ 
-console.log("writing code within VS Code");
-//mongoose.connect(process.env.DB_URI);
+mongoose.connect(process.env.MONGO_URI,{
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
 
+//not sure much about cors yet, this was here so I'm leaving it
 app.use(cors());
 
-/** this project needs to parse POST bodies **/
+// this project needs to parse POST bodies **/
 // you should mount the body-parser here
+app.use(bodyParser.urlencoded({extended: false}));
+
+/** # SCHEMAS and MODELS #
+ *  ======================
+ */
+var Schema = mongoose.Schema;
+
+var urlSchema = new Schema({
+  url: { type: String, required: true },
+  num: { type: Number }
+});
+
+const Url = mongoose.model("Url",urlSchema);
+
+/** # MONGOOSE FUNCTIONS #
+ *  ======================
+ */
+
+ var createAndSaveUrl = function(urlIn, done){
+   getDocumentCount((d)=>{
+     const index = d+1;
+     var mySite = new Url({
+       url: urlIn,
+       num: index
+     })
+     mySite.save(function(err,data){
+      if(err) return console.error(err);
+      console.log("Passing in this data...");
+      console.log(data);
+      let responseObject = {
+        original_url: urlIn,
+        short_url: data.num
+      }
+      done(null,responseObject);
+    })
+   })
+ }
+
+ var getDocumentCount = function(done){
+   return Url.countDocuments({}, function(err,count){
+    done(count);
+   });
+ }
+
+ var findSiteAtIndex = function(numIn, done){
+   Url.findOne({num: numIn},(err, data) => {
+    if (err) return console.log(err); 
+    console.log(data);
+    var sanitizedUrl = data.url;
+    if(sanitizedUrl.substring(0,4)!="http"){
+      sanitizedUrl = "http://" + sanitizedUrl;
+    }
+     done(sanitizedUrl);
+   })
+ }
 
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function(req, res){
   res.sendFile(process.cwd() + '/views/index.html');
 });
-
   
 // your first API endpoint... 
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+//----- me testing crap -------
 //var createAndSaveUrl = require('./myApp.js').createAndSaveUrl;
 // the process.env.TEST_VAR DOES properly log. Not sure wtf is going on.
-var localRefToFun = require('./myApp.js').createAndSaveUrl;
 app.get("/api/customTest", function(req, res){
-  let testCrossFile = "test";
-  testCrossFile = localRefToFun();
-  res.json({message: testCrossFile});
+  let resp = createAndSaveUrl();
+  res.json({message: resp})
+});
+
+app.get("/api/count", function(req, res){
+  getDocumentCount((d)=> {
+    res.json({dbCount: d})
+  });
 })
 
-// router.post("/api/shorturl/new", function(req, res){
-  
-// })
+app.get("/api/redirect", function (req, res){
+  console.log({"key":"vaaaaalue","key2":"valuuuuue"});
+  res.redirect("http://www.aarongilly.com");
+})
 
+//----- the actual project -----
+
+app.post("/api/shorturl/new",  function(req, res){
+  let receivedUrl = req.body.url;
+  dns.lookup(receivedUrl,(err, address, family) => {
+    if(err !== null){
+      res.json({"error":"invalid URL"})
+    }else{
+      console.log("err " + err);
+      console.log("address " + address);
+      createAndSaveUrl(receivedUrl,(e,d) => {
+        res.json(d);
+      })
+    }
+  })
+})
+
+app.get("/api/shorturl/:num",(req, res)=>{
+  findSiteAtIndex(req.params.num,(d)=>{
+    res.redirect(d);
+  })
+})
 
 app.listen(port, function () {
   console.log('Node.js listening ...');
